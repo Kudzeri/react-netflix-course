72b105dc45a986b55dd8f8c85653514d6a24eb23
Kudzeri <kudzeri@gmail.com>
2025-06-24 22:34:06 +0500

    Update for Render

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 4173,
+    host: true,
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true,
+  },
   plugins: [react(), tailwindcss()],
-})
+});
